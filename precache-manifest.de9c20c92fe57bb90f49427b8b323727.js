cead3352b40679a763e5f748bbfdaeb6819ca33c
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2020-01-22 12:20:12 -0600

    Updates

---
+++ precache-manifest.de9c20c92fe57bb90f49427b8b323727.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b5caafa948f5240e616277aedd71944b",
+    "url": "/shopping-react-practices/index.html"
+  },
+  {
+    "revision": "7b20d3479942a76f2a36",
+    "url": "/shopping-react-practices/static/css/main.504b344e.chunk.css"
+  },
+  {
+    "revision": "ea8d41dff83e2fd97d63",
+    "url": "/shopping-react-practices/static/js/2.09c75148.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/shopping-react-practices/static/js/2.09c75148.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b20d3479942a76f2a36",
+    "url": "/shopping-react-practices/static/js/main.007ba2ac.chunk.js"
+  },
+  {
+    "revision": "4193e0357b057c99edbb",
+    "url": "/shopping-react-practices/static/js/runtime-main.848395c7.js"
+  }
+]);
