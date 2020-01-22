f0e11c7baaa38c5493dd6eaac4dd3ae957494568
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2020-01-22 11:03:20 -0600

    adding function to delete item from the shopping car

--- src/components/Product.js
+++ src/components/Product.js
@@ -3,21 +3,37 @@ import './style.css';
 
 const Product = ({ product, car, setCar, products }) => {
 
-  const {name, price, id} = product
-
   const ProductSelected = (id) => {
-    const items = products.filter(item => item.id === id);
+    const items = products.filter(item => item.id === id)[0];
     setCar([
       ...car,
-      ...items
+      items
     ]);
   }
+
+  const ProductDeleted = (id) => {
+    const items = car.filter(item => item.id !== id);
+    setCar(items)
+  }
+
+  const {name, price, id} = product;
 
   return ( 
     <div className='product-div'>
       <h3>{name}</h3>
       <p>${price}</p>
-      <button onClick={() => {ProductSelected(id)}} className='btn-buy'>Comprar</button>
+
+      {
+        products
+        ?
+        ( 
+          <button onClick={() => {ProductSelected(id)}} className='btn-buy'>Comprar</button>
+        )
+        :
+        (
+          <button onClick={() => {ProductDeleted(id)}} className='btn-buy'>Eliminar</button>
+        )
+      }
     </div>
    );
 }
